a2359d72e49e79d7a933cae5ffaeb88a11a7684c
Mikhail Novikov <thepocp@gmail.com>
2023-02-11 20:59:52 +0400

    Add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { FC, useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+import reactLogo from './assets/react.svg';
+
+import './App.css';
+
+export const App: FC = () => {
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        <a href="https://vitejs.dev" rel="noreferrer" target="_blank">
+          <img alt="Vite logo" className="logo" src="/vite.svg" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://reactjs.org" rel="noreferrer" target="_blank">
+          <img alt="React logo" className="logo react" src={reactLogo} />
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -28,7 +30,5 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
-}
-
-export default App
+  );
+};
