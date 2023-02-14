2f4140cbd9a2dd93bd6637a5534260e88968c8de
Mikhail Novikov <thepocp@gmail.com>
2023-02-15 00:28:37 +0400

    Change card layout

--- src/components/CharacterCard/index.tsx
+++ src/components/CharacterCard/index.tsx
@@ -19,10 +19,26 @@ export const CharacterCard: FC<Props> = ({ character, showCharacterInfo }) => (
   <Grid height="320px" item lg={3} md={4} sm={6} xs={6}>
     <Card
       onClick={(): void => showCharacterInfo(character.id || '')}
-      sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+      sx={{
+        height: '100%',
+        '&:hover': {
+          transform: 'scale(1.1)',
+          transition: 'transform 0.3s',
+        },
+      }}
     >
-      <CardActionArea sx={{ height: '100%' }}>
-        <CardMedia alt="random" component="img" image={character.image || ''} />
+      <CardActionArea
+        sx={{
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <CardMedia
+          alt={character.name || ''}
+          component="img"
+          image={character.image || ''}
+        />
         <CardContent sx={{ flexGrow: 1 }}>
           <Typography>{character.name}</Typography>
         </CardContent>
